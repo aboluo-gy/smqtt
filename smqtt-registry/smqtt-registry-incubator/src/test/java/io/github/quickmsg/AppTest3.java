af40cbaafdfc4f64ad3178845821b0a38694ace7
luxurong <1074455781@qq.com>
2021-05-22 22:47:16 +0800

    md

---
+++ smqtt-registry/smqtt-registry-incubator/src/test/java/io/github/quickmsg/AppTest3.java
@@ -0,0 +1,64 @@
+package io.github.quickmsg;
+
+import com.codahale.metrics.MetricRegistry;
+import org.apache.gossip.GossipMember;
+import org.apache.gossip.GossipService;
+import org.apache.gossip.GossipSettings;
+import org.apache.gossip.RemoteGossipMember;
+import org.apache.gossip.event.GossipListener;
+import org.apache.gossip.event.GossipState;
+import org.apache.gossip.model.SharedGossipDataMessage;
+import org.junit.Test;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest3
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() throws URISyntaxException, UnknownHostException, InterruptedException {
+
+        GossipSettings settings = new GossipSettings();
+        List<GossipMember> startupMembers = new ArrayList<>();
+        URI uri2 = new URI("udp://127.0.0.1:5001" );
+        startupMembers.add(new RemoteGossipMember("cluster", uri2, "1"));
+        URI uri3 = new URI("udp://127.0.0.1:5002" );
+        startupMembers.add(new RemoteGossipMember("cluster", uri3, "2"));
+        URI uri = new URI("udp://" + "127.0.0.1" + ":" + 5003);
+        GossipService gossipService = new GossipService("cluster", uri,   "3",null,
+                startupMembers, settings, new GossipListener() {
+            @Override
+            public void gossipEvent(GossipMember member, GossipState state) {
+                System.out.println(member+state.name());
+            }
+        }, new MetricRegistry());
+        gossipService.start();
+        SharedGossipDataMessage sendSharedGossipDataMessage= new SharedGossipDataMessage();
+        sendSharedGossipDataMessage.setPayload("我來測試一下3");
+        sendSharedGossipDataMessage.setKey("test3");
+        sendSharedGossipDataMessage.setTimestamp(System.currentTimeMillis());
+        sendSharedGossipDataMessage.setExpireAt(Long.MAX_VALUE);
+        gossipService.gossipSharedData(sendSharedGossipDataMessage);
+        for (;;){
+            Thread.sleep(1000);
+            SharedGossipDataMessage sharedGossipDataMessage2 = gossipService.findSharedData("test2");
+            if(sharedGossipDataMessage2!=null) {
+                System.out.println("test2:"+sharedGossipDataMessage2.getPayload());
+            }
+            SharedGossipDataMessage sharedGossipDataMessage1 = gossipService.findSharedData("test1");
+            if(sharedGossipDataMessage1!=null) {
+                System.out.println("test1:"+sharedGossipDataMessage1.getPayload());
+            }
+        }
+
+    }
+}
